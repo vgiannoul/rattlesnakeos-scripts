01b74487f52c658db63051af32589a7b515cc7e9
Vasilis Giannoulis <vgiannoul@gmail.com>
2020-03-09 09:45:44 +0200

    Fix boot-animation and add Ubuntu font

--- 00002-replace-default-font.sh
+++ 00002-replace-default-font.sh
@@ -1,6 +1,6 @@
  #!/bin/bash
 
-FONTS_ZIP_FILE="https://github.com/vgiannoul/rattlesnakeos-scripts/raw/master/files/GoogleSans.zip"
+FONTS_ZIP_FILE="https://github.com/vgiannoul/rattlesnakeos-scripts/raw/master/files/Ubuntu.zip"
 mkdir -p $BUILD_DIR/branding
 echo "Downloading fonts zip file $FONTS_ZIP_FILE"
 wget -O $BUILD_DIR/branding/custom_font.zip $FONTS_ZIP_FILE
